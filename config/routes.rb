f5b29d45fbb1adce2d568a00019490d7ccaa27db
Ayumu Kojima <81469999+AyumuKojima@users.noreply.github.com>
2022-07-20 11:40:57 +0900

    fix: ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  
+  root to: "years#index"
+  resources :years, only: :index do
+    resources :months, only: :index do
+      resources :spends, only: :index
+    end
+  end
 end

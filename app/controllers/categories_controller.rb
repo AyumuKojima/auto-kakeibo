729898e206ecff35a42b49af5dcf9b9eacd7bba6
Ayumu Kojima <81469999+AyumuKojima@users.noreply.github.com>
2022-07-30 23:49:23 +0900

    add: カテゴリ詳細画面を作成

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,9 @@
 class CategoriesController < ApplicationController
-  before_action :year_month_params, only: :index
+  before_action :year_month_params
   def index
+  end
+
+  def show
   end
 
   private
@@ -9,7 +12,11 @@ class CategoriesController < ApplicationController
     @year = params[:year_id].to_i
     @month = params[:month_id].to_i
     if @year < 2000 || @month < 1 || @month > 12
-      redirect_to year_month_categories_path(Date.today.year, Date.today.month)
+      if request.path == "/years/#{@year}/months/#{@month}/categories"
+        redirect_to year_month_categories_path(Date.today.year, Date.today.month)
+      elsif request.path == "/years/#{@year}/months/#{@month}/categories/#{params[:id]}"
+        redirect_to year_month_category_path(Date.today.year, Date.today.month)
+      end
     end
   end
 end
